27d9996b94bf78e5ad343f372ed4d16c20f97577
Yari-Ros <2118088@talnet.nl>
2019-11-15 13:02:29 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,7 @@
+<?php
+
+$FirstName=readline ("What is your first name, young one.");
+echo "ah.. " .$FirstName ."what an unusual name to have..";
+$LastName=readline ("And your last name, i'must know, what is it?");
+echo $FirstName;
+echo " " .$Lastname (".. ah, thanks for telling me, I shall let the counsil know.");
